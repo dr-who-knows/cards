245c0a96ac0ff2211f39bc1ed9bb26e905c3aaaa
Valery <valery@email.com>
2025-03-23 12:48:10 +0100

    canvas fix

--- cards/flashcards/admin.py
+++ cards/flashcards/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
 from .models import Card, UserProfile
 
-@admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     list_display = ('original_word', 'translation', 'date_added')
     search_fields = ('original_word', 'translation')
     list_filter = ('date_added',)
     ordering = ('-date_added',)
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'canvas_translate_x', 'canvas_translate_y', 'canvas_scale')
+    search_fields = ('user__username', 'user__email')
+    list_filter = ('canvas_scale',)
+    
+    fieldsets = (
+        ('User Information', {
+            'fields': ('user',)
+        }),
+        ('Canvas Position', {
+            'fields': ('canvas_translate_x', 'canvas_translate_y', 'canvas_scale'),
+            'description': 'These values control the canvas view position for this user'
+        }),
+    )
+
+admin.site.register(Card, CardAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
